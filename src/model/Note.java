4f2f89da5e1a1d52e6986e6f49e559307740b56d
Aswin <aswinvinod336@gmail.com>
2025-09-16 15:12:39 +0530

    Model updated: added fields, getters, setters, and constructors

--- src/model/Note.java
+++ src/model/Note.java
@@ -1,4 +1,53 @@
 package model;
 
+import java.util.Date;
+
 public class Note {
+    private String id;
+    private String title;
+    private String description;
+    private Date date;
+
+    public Note() {
+    }
+
+    public Note(String id, String title, String description, Date date) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.date = date;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
 }
